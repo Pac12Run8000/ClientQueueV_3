fc9905b18081a627876bc5d2b2d2c0cf067bf672
Pac12Run8000 <gangstarr78992@>
2021-08-20 22:08:33 -0700

    Adding the client signupview

---
+++ ClientQueueV_3/Views/ClientSignupView.swift
@@ -0,0 +1,15 @@
+//
+//  ClientSignupView.swift
+//  ClientQueueV_3
+//
+//  Created by Michelle Grover on 8/20/21.
+//
+
+import UIKit
+
+class ClientSignupView: UIView {
+
+    @IBOutlet var view: UIView!
+    
+
+}
