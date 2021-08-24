fcf21693b22e15421c5a61469f82d52a49546bfe
Pac12Run8000 <gangstarr78992@>
2021-08-24 10:19:36 -0700

    back to a working state

---
+++ ClientQueueV_3/CustomObjects/KeyboardNotification.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardNotification.swift
+//  ClientQueueV_3
+//
+//  Created by Michelle Grover on 8/23/21.
+//
+
+import UIKit
+
+struct KeyboardNotification {
+    var controller:UIViewController
+    
+    init(controller:UIViewController) {
+        self.controller = controller
+    }
+
+}
